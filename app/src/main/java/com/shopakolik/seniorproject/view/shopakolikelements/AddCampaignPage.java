6b5d26b07d11222b63e37579bef49a06b5e0ebef
iremisiltekes <iremisil92@gmail.com>
2015-04-10 22:13:15 +0300

    add campaign page

---
+++ app/src/main/java/com/shopakolik/seniorproject/view/shopakolikelements/AddCampaignPage.java
@@ -0,0 +1,11 @@
+package com.shopakolik.seniorproject.view.shopakolikelements;
+
+/**
+ * Created by IREM on 4/10/2015.
+ */
+public class AddCampaignPage {
+
+    // datepicker yapilacak && textfield la baglanacak
+    // photo yu kullanicinin yüklemesi saglanacak addphoto butonuna eklenecek
+}
+
