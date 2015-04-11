7e2e53fed655a046a7450abda6fd076b5e2cad7a
zeuyanik <zeuyanik@gmail.com>
2015-04-11 19:07:13 +0300

    database'den categories cekildi
    customer suscribe ve categorypage icin

--- app/src/main/java/com/shopakolik/seniorproject/view/shopakolikelements/CategoryPage.java
+++ app/src/main/java/com/shopakolik/seniorproject/view/shopakolikelements/CategoryPage.java
@@ -1,51 +1,61 @@
 package com.shopakolik.seniorproject.view.shopakolikelements;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.SparseBooleanArray;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.shopakolik.seniorproject.R;
+import com.shopakolik.seniorproject.controller.databasecontroller.DatabaseManager;
+import com.shopakolik.seniorproject.model.shopakolikelements.Category;
+
+import java.util.ArrayList;
 
 /**
- * Created by zeyno on 2/12/2015.
+ * Created by zeyno on 4/11/2015.
  */
 public class CategoryPage extends Activity {
     ListView lv;
     Button button;
+    // String categoryType [] = {"Woman" , "Man" , "Kids", "Shoes", "Accessory", "Handbags, Wallets & Cases", " Jewelry" ,"Books", " DVDs & Movies","Sport", "Baby"};
 
-    String categoryType [] = {"Woman" , "Man" , "Kids", "Shoes", "Accessory", "Handbags, Wallets & Cases", " Jewelry" ,"Books", " DVDs & Movies","Sport", "Baby"};
+    // String categoryType1 [] = {"ekleme"};
+    ArrayList<Category> categoryType;
+    String categories[];
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.categorypage);
 
-        lv =(ListView)findViewById(R.id.categories_list);
-        button=(Button)findViewById(R.id.categories_submit_button);
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                DatabaseManager db = new DatabaseManager();
+                try {
+                    categoryType = db.getCategoryList("mmmn", "mn");
 
-        ArrayAdapter<String> ard = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice, categoryType);
-        lv.setAdapter(ard);
-        lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-
-    }
-
-
-    public void categoriesSubmitButtonClick(View view){
-        SparseBooleanArray sba =lv.getCheckedItemPositions();
-            for(int i=0; i < sba.size();i++){
-                if(sba.valueAt(i)){
-                    Toast.makeText(getApplicationContext(), categoryType[sba.keyAt(i)], Toast.LENGTH_SHORT).show();
-
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
+        });
+        t.start();
+        try {
+            t.join();
 
+            String categories[] = new String[categoryType.size()];
+            for (int i = 0; i < categories.length; i++) {
+                categories[i] = categoryType.get(i).getName();
+            }
+            lv = (ListView) findViewById(R.id.categories_list);
+            button = (Button) findViewById(R.id.categories_submit_button);
+
+            ArrayAdapter<String> ard = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, categories);
+            lv.setAdapter(ard);
+            lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-
-
 }
