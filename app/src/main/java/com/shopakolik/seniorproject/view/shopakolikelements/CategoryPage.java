298c13d2ebe2c1f3faec4f0cccd81e0788f2d945
zeuyanik <zeuyanik@gmail.com>
2015-04-11 12:53:33 +0300

    camera roll'a girdi

--- app/src/main/java/com/shopakolik/seniorproject/view/shopakolikelements/CategoryPage.java
+++ app/src/main/java/com/shopakolik/seniorproject/view/shopakolikelements/CategoryPage.java
@@ -15,7 +15,7 @@ public class CategoryPage extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signupcustomer);
+        setContentView(R.layout.categorypage);
         populateListView();
     }
 
--- app/src/main/java/com/shopakolik/seniorproject/view/shopakolikelements/SignUpForShop.java
+++ app/src/main/java/com/shopakolik/seniorproject/view/shopakolikelements/SignUpForShop.java
@@ -2,8 +2,12 @@ package com.shopakolik.seniorproject.view.shopakolikelements;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.shopakolik.seniorproject.R;
 
@@ -16,10 +20,40 @@ public class SignUpForShop extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signupshop);
 
+        iv=(ImageView)findViewById(R.id.imageView);
+
     }
 
     public void categoriesClick(View view){
         Intent getNameScreenIntent = new Intent(this,CategoryPage.class);
         startActivity(getNameScreenIntent);
+    }
+
+    private static final int SELECTED_PICTURE=1;
+    ImageView iv;
+
+    public void pickimageclicked(View view){
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, SELECTED_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(resultCode){
+            case SELECTED_PICTURE:
+                if(resultCode==RESULT_OK){
+                    Uri uri = data.getData();
+                    String[] projection = {MediaStore.Images.Media.DATA};
+                    Cursor cursor = getContentResolver().query(uri,projection,null, null,null);
+                    cursor.moveToFirst();
+                    int coloumnIndex= cursor.getColumnIndex(projection[0]);
+                    String filePath= cursor.getString(coloumnIndex);
+                    cursor.close();
+
+
+                }
+        }
     }
 }
